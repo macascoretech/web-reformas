2c3927fb871a8d06746c9fbe21a88893a78563b8
Anthony Garcia <tecnicos@macascore.com>
2026-03-18 19:51:01 +0100

    fix something more

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,7 +8,7 @@ export default defineConfig({
     integrations: [
         tailwind(),
     ],
-    output: 'hybrid',
+    output: 'server',
     adapter: vercel(),
 
     server: {
