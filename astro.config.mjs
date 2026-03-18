05b91316b19bbc5892cdcc6846f6e92ec458e0e5
Anthony Garcia <tecnicos@macascore.com>
2026-03-18 19:26:34 +0100

    fix something more

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
@@ -8,9 +7,6 @@ export default defineConfig({
     site: 'https://macascore.com',
     integrations: [
         tailwind(),
-        sitemap({
-            filter: (page) => !page.includes('/api/'),
-        }),
     ],
     output: 'hybrid',
     adapter: vercel(),
